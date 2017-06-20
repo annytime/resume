a3dcf6693023d67cef002d120fd42dc9aac3bb69
annytime <yaoosh@foxmail.com>
2017-06-20 11:33:23 +0800

    delete

--- music-player/js/toolFunction.js
+++
@@ -1,7 +0,0 @@
-
-//毫秒转00:00格式
-function transformTime(time){
-    var higher=parseInt(time/60)<10?("0"+parseInt(time/60)):parseInt(time/60);
-    var lower=parseInt(time%60)<10?("0"+parseInt(time%60)):parseInt(time%60);
-    return  higher+":"+lower;
-}
